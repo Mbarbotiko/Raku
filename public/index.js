f11c653e1d81407f28b059806f2a21520faf342c
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-01-23 07:33:22 -0600

    adding other HTML pages, and testing event listeners for load function

--- public/index.js
+++ public/index.js
@@ -1,1 +1,33 @@
-console.log('javascript file linked');
+$(function () {
+    console.log('javascript file linked');
+
+    let locationLink = document.querySelectorAll('[data-value ="location"]')[0];
+
+    let homeLink = document.querySelectorAll('[data-value ="home"]')[0];
+
+    let careersLink = document.querySelectorAll('[data-value ="careers"]')[0];
+
+
+    locationLink.addEventListener('click', function(){
+        $('.container').load('./location.html');
+
+    });
+
+    homeLink.addEventListener('click', function(){
+        $('.container').load('./index.html');
+
+    });
+
+    careersLink.addEventListener('click', function(){
+        $('.container').load('./careers.html');
+
+    });
+   
+    //CORS error must be on a server local host or online already.
+
+    // http://api.jquery.com/load/
+
+
+ 
+
+});
