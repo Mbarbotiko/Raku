685e4363660a4e697695dc8402ce362d0744063f
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-02-02 13:01:20 -0600

    adding http request for nav on nav js file linked to other pages

--- public/nav.js
+++ public/nav.js
@@ -1,9 +1,24 @@
 $(function () {
-    console.log('javascript file linked');
-
-    if(window.location.pathname!='/'){
-        console.log('nav is linked')
+    console.log('nav file linked');
+    //start new HTTP request
+    var xhr = new XMLHttpRequest();
+    xhr.onload= function(){
+        //if a successful request:
+        if (xhr.status ===200){
+            responseObj = xhr.responseText
+            var parser, xmlDoc
+            //use DOMParser to find element from response to replace innerHTML of current page
+            parser = new DOMParser();
+            xmlDoc = parser.parseFromString(responseObj, 'text/html')
+            ('loader-nav')
+            document.getElementById('testing').innerHTML= xmlDoc.getElementById
+            ('loader-nav').innerHTML;
+            //remember to change the testing ID when other pages are built to a different ID.
+        }
     }
 
+
+xhr.open('GET', 'index.html', true);
+xhr.send(null);
     
 });
