90bd4a00911f664b15c440dfbcdeb09b4a9ba16a
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-02-02 13:11:46 -0600

    fixing request

--- public/nav.js
+++ public/nav.js
@@ -1,22 +1,18 @@
 $(function () {
     console.log('nav file linked');
-    //start new HTTP request
+    //create new HTTP req
     var xhr = new XMLHttpRequest();
     xhr.onload= function(){
-        //if a successful request:
+        //if response is good
         if (xhr.status ===200){
             responseObj = xhr.responseText
             var parser, xmlDoc
-            //use DOMParser to find element from response to replace innerHTML of current page
             parser = new DOMParser();
             xmlDoc = parser.parseFromString(responseObj, 'text/html')
-            ('loader-nav')
             document.getElementById('testing').innerHTML= xmlDoc.getElementById
-            ('loader-nav').innerHTML;
-            //remember to change the testing ID when other pages are built to a different ID.
+            ('loader-nav').innerHTML
         }
     }
-
 
 xhr.open('GET', 'index.html', true);
 xhr.send(null);
