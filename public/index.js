fce0c2dba67954badf9a12e697eb200804811986
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-02-09 12:56:48 -0600

    adding fixed nav bar in index js class list, changed css for styling navbar and the image

--- public/index.js
+++ public/index.js
@@ -1,27 +1,19 @@
 $(function () {
     console.log('index.js ran');
-
-    // if(window.location.pathname!=='/'){
-    //     console.log('heybuddy')
-    // }
-
- 
-//#loader-nav, .hd-ft-bar
-    headerBar= document.getElementById('loader-nav').getElementsByClassName('hd-ft-bar')[0];
-    console.log(headerBar)
+    headerID = document.getElementById('loader-nav')
+    headerBar= headerID.getElementsByClassName('hd-ft-bar')[0];
+    navBar = headerID.getElementsByClassName('navbar')[0]
 
    function headerBarHide(){
-        console.log(window.scrollY);
+       //function hides the address bar at the top of the screen on scroll, to add a fade effect write below:
         if(window.scrollY>0){
             headerBar.style.display ="none"
+            navBar.classList.add('fixed-top')
         }else{
             headerBar.style.display ="block"
+            navBar.classList.remove('fixed-top');
         }
     };
-
    window.addEventListener('scroll', headerBarHide)
-
-
-  
 
 });
