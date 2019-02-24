f6e728b62bb8e54126dcda557726a1a5e4ce2aea
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-02-24 09:21:21 -0600

    adding js position info for about us wrapper and function to change css to fade and slide in text

--- public/index.js
+++ public/index.js
@@ -1,34 +1,49 @@
 $(function () {
     console.log('index.js ran');
-    headerID = document.getElementById('loader-nav')
-    headerBar= headerID.getElementsByClassName('hd-ft-bar')[0];
-    navBar = headerID.getElementsByClassName('navbar')[0]
+    headerID = document.getElementById('loader-nav');
+    headerBar = headerID.getElementsByClassName('hd-ft-bar')[0];
+    navBar = headerID.getElementsByClassName('navbar')[0];
 
-   function headerBarHide(){
-       //function hides the address bar at the top of the screen on scroll, to add a fade effect write below:
-        if(window.scrollY>0){
-            headerBar.style.display ="none"
-            navBar.classList.add('fixed-top')
-        }else{
-            headerBar.style.display ="block"
+    function headerBarHide() {
+        //Function hides the address bar at the top of the screen on scroll, to add a fade effect write below:
+        if (window.scrollY > 0) {
+            headerBar.style.display = "none";
+            navBar.classList.add('fixed-top');
+        } else {
+            headerBar.style.display = "block";
             navBar.classList.remove('fixed-top');
         }
     };
-   window.addEventListener('scroll', headerBarHide)
+    //Event listener for window on scroll to run headerBarHide();
+    window.addEventListener('scroll', headerBarHide);
 
-   console.log($('.middle-image').height());
+//Select the element class
+let aboutUsWrapper = document.getElementsByClassName('aboutus-wrapper')[0];
+console.log(aboutUsWrapper);
 
-   var a = document.getElementsByClassName('middle-image')[0].clientHeight;
-var b = document.getElementsByClassName('middle-image')[0].offsetHeight;
-var c = document.getElementsByClassName('middle-image')[0].scrollHeight;
-console.log(a,b,c);
+function aboutusWrapperInView(e){
+    var elinfo = {
+        "top":aboutUsWrapper.offsetTop,
+        "height":aboutUsWrapper.offsetHeight,
+    };
 
-//add event listener to remove class and add class in javascript instead: DO NOT TOGGLE THE CLASS
-//left side of about me panel:
-    $('.right-side').click(function(){
-      $('.left-side').toggleClass('left-side-new');
-    });
+    if (elinfo.top + elinfo.height < window.pageYOffset || elinfo.top > window.pageYOffset + window.innerHeight) {
+        console.log('I am not in view')
+        $('.overlay-about div').css('opacity', 1);
+        return false;
+    } else {
+        console.log('I am in view')
+        $('.left-side').toggleClass('left-side-new');
+        $('.right-side').toggleClass('right-side-new');
+        $('.overlay-about div').css('opacity', 0);
+        return true;
+    }
+}
 
+//When any element in that class becomes visible in the middle of the screen fade and roll in the text
 
+window.addEventListener('scroll', aboutusWrapperInView)
+;
+//When it is no longer visible in the middle of the screen/ view port toggle the class back.
 
 });
