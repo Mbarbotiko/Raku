74f6b4d361088ba6bda26fdf9ff5f3067e9b0504
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-02-24 18:21:30 -0600

    adding page titles, footer Margarita created by, css and added logo

--- public/index.js
+++ public/index.js
@@ -14,22 +14,22 @@ $(function () {
             navBar.classList.remove('fixed-top');
         }
     };
-    //Event listener for window on scroll to run headerBarHide();
+    //Event listener for window on scroll for header footer bar
     window.addEventListener('scroll', headerBarHide);
+    //Event listener for window on scroll for about us section
+    window.addEventListener('scroll', aboutusWrapperInView);
 
-    //Select the element class
+    //Select wrapper for about us section
     let aboutUsWrapper = document.getElementsByClassName('aboutus-wrapper');
-
+    //Function for removing and adding classes on scroll for the wrapper
     function aboutusWrapperInView() {
-
         for (var i = 0; i < aboutUsWrapper.length; i++) {
-
-            if(aboutUsWrapper[i].offsetTop+aboutUsWrapper[i].offsetHeight<window.pageYOffset-400||aboutUsWrapper[i].offsetTop>window.pageYOffset-400 + window.innerHeight){
+            if (aboutUsWrapper[i].offsetTop + aboutUsWrapper[i].offsetHeight < window.pageYOffset - 400 || aboutUsWrapper[i].offsetTop > window.pageYOffset - 400 + window.innerHeight) {
                 $(aboutUsWrapper[i]).children('.left-side').removeClass('left-side-new');
                 $(aboutUsWrapper[i]).children('.right-side').removeClass('right-side-new');
                 $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').removeClass('disappear')
-            }else{
-                 $(aboutUsWrapper[i]).children('.left-side').addClass('left-side-new');
+            } else {
+                $(aboutUsWrapper[i]).children('.left-side').addClass('left-side-new');
                 $(aboutUsWrapper[i]).children('.right-side').addClass('right-side-new');
                 $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').addClass('disappear')
 
@@ -37,10 +37,5 @@ $(function () {
         }
     }
 
-    //When any element in that class becomes visible in the middle of the screen fade and roll in the text
-
-    window.addEventListener('scroll', aboutusWrapperInView)
-        ;
-    //When it is no longer visible in the middle of the screen/ view port toggle the class back.
 
 });
