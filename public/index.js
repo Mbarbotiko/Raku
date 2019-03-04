5cc43cbac259b40eaefb3c4d850f06f5fc0123a0
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-03-04 07:48:28 -0600

    adding javascript to hide panels at media screen sie, adding resizing of panels in CSS for about section

--- public/index.js
+++ public/index.js
@@ -23,15 +23,30 @@ $(function () {
     let aboutUsWrapper = document.getElementsByClassName('aboutus-wrapper');
     //Function for removing and adding classes on scroll for the wrapper
     function aboutusWrapperInView() {
-        for (var i = 0; i < aboutUsWrapper.length; i++) {
-            if (aboutUsWrapper[i].offsetTop + aboutUsWrapper[i].offsetHeight < window.pageYOffset - 400 || aboutUsWrapper[i].offsetTop > window.pageYOffset - 400 + window.innerHeight) {
-                $(aboutUsWrapper[i]).children('.left-side').removeClass('left-side-new');
-                $(aboutUsWrapper[i]).children('.right-side').removeClass('right-side-new');
-                $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').removeClass('disappear')
-            } else {
-                $(aboutUsWrapper[i]).children('.left-side').addClass('left-side-new');
-                $(aboutUsWrapper[i]).children('.right-side').addClass('right-side-new');
-                $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').addClass('disappear')
+        //if window width is equal to or higher than __pixels, run this code:
+        if (window.innerWidth >= 1160) {
+            for (var i = 0; i < aboutUsWrapper.length; i++) {
+                if (aboutUsWrapper[i].offsetTop + aboutUsWrapper[i].offsetHeight < window.pageYOffset - 400 || aboutUsWrapper[i].offsetTop > window.pageYOffset - 400 + window.innerHeight) {
+                    $(aboutUsWrapper[i]).children('.left-side').removeClass('left-side-new');
+                    $(aboutUsWrapper[i]).children('.right-side').removeClass('right-side-new');
+                    $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').removeClass('disappear')
+                } else {
+                    $(aboutUsWrapper[i]).children('.left-side').addClass('left-side-new');
+                    $(aboutUsWrapper[i]).children('.right-side').addClass('right-side-new');
+                    $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').addClass('disappear')
+
+                }
+            }
+        } if (window.innerWidth < 1159) {
+            $(aboutUsWrapper[i]).children('.left-side').css('display', 'none');
+            $(aboutUsWrapper[i]).children('.right-side').css('display', 'none');
+            for (var i = 0; i < aboutUsWrapper.length; i++) {
+                if (aboutUsWrapper[i].offsetTop + aboutUsWrapper[i].offsetHeight < window.pageYOffset - 400 || aboutUsWrapper[i].offsetTop > window.pageYOffset - 400 + window.innerHeight) {
+                    $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').removeClass('disappear')
+                } else {
+                    $(aboutUsWrapper[i]).children('.panel.middle').find('.overlay-header').addClass('disappear')
+
+                }
 
             }
         }
