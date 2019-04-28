f7aafa03afbce31918c31d301d750f442ca55c71
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-04-28 08:42:26 -0500

    deleting

--- config.js
+++ config.js
@@ -5,6 +5,3 @@ module.exports = {
   port: process.env.PORT
 };
 
-// var iFrame = `<iframe width="50%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=${googleMapsKey}
-// &q=Raku+Sushi,Saint Louis Park+MN" allowfullscreen>
-// </iframe>`
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,7 +22,7 @@ app.get("/", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/staff.html"));
   // })
 
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
 };
