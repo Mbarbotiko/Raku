b99ab43906b18fa3cc5ed6b111f85906b6bdf33e
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-06-30 13:07:31 -0500

    adding toggle function to hidden menu

--- public/menu.js
+++ public/menu.js
@@ -1,4 +1,4 @@
-$('.menu-list').hide();
+$('#happyhour').hide();
 
 //rewrite to be one function passed params
 (function loadItems() {
@@ -42,38 +42,34 @@ $('.menu-list').hide();
     }
 
     //maybe consolidate this later add headers using jscript instead so this function is only called per section instead of per header in each section
-    loadItems(happyHourApps, '.appetizer')
-    loadItems(happyHourSushiRaw, '.app-raw')
-    loadItems(happyHourSushiRolls, '.app-rolls')
-    loadItems(happyHourSpRolls, '.app-specialty-rolls')
-    loadItems(drinkSpecials, '.drink-specials')
-    loadItems(lunchMenuEntree, '.lunch-entrees')
-    loadItems(lunchSpecialRolls, '.lunch-specialty-rolls')
-    loadItems(lunchBox, '.lunch-boxes');
-    loadItems(dinnerEntree, '.dinner-entrees')
-    loadItems(dinnerRolls, '.dinner-specialty-rolls')
+    loadItems(happyHourApps, '#happyhour')
+    // loadItems(happyHourSushiRaw, '.app-raw')
+    // loadItems(happyHourSushiRolls, '.app-rolls')
+    // loadItems(happyHourSpRolls, '.app-specialty-rolls')
+    // loadItems(drinkSpecials, '.drink-specials')
+    // loadItems(lunchMenuEntree, '.lunch-entrees')
+    // loadItems(lunchSpecialRolls, '.lunch-specialty-rolls')
+    // loadItems(lunchBox, '.lunch-boxes');
+    // loadItems(dinnerEntree, '.dinner-entrees')
+    // loadItems(dinnerRolls, '.dinner-specialty-rolls')
 
 
 }());
 
 
 
-$('.arrow.down').on('click', function () {
-    var menuItemsText = $(this).siblings('.menu-list');
-    menuItemsText.toggle();
-    var upArrow = $(this).siblings('.arrow.up');
-    upArrow.toggle();
-
+$('#happy-hour-button').on('click', function () {
+   $('#happyhour').toggle();
 })
 
 
 
-$('.arrow.up').on('click', function () {
-    $(this).hide();
-    var menuItemsText = $(this).siblings('.menu-list');
-    menuItemsText.toggle();
+// $('.arrow.up').on('click', function () {
+//     $(this).hide();
+//     var menuItemsText = $(this).siblings('.menu-list');
+//     menuItemsText.toggle();
 
 
-})
+// })
 
 
