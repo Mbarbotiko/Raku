c3a75540d2edc8554ea0a45f0a0d7e3e95ae3d9f
Margarita Barbotiko <mbarbotiko@gmail.com>
2019-09-15 11:24:44 -0500

    creating media buttons instead of messing with click events

--- public/menuDesktop.js
+++ public/menuDesktop.js
@@ -1,5 +1,16 @@
-//Declaring functions:
-
+//Global vars
+//screen break considered media screen change
+// const desktopView = 866;
+// const mobileView = 865;
+function hideMenus() {
+    $('#dt-happyhour').hide();
+    $('#dt-lunch').hide();
+    $('#dt-dinner').hide();
+    $('#m-happyhour').hide();
+    $('#m-lunch').hide();
+    $('#m-dinner').hide();
+}
+hideMenus();
 //function that loads menuobject into the dom
 function loadItems(obj, cssName) {
     var s = '<span>';
@@ -40,134 +51,86 @@ function loadItems(obj, cssName) {
     }
 }
 
-//Global vars
-//screen break considered media screen change
-const desktopView = 866;
-const mobileView = 865;
+loadItems(happyHour, '#dt-happyhour');
+loadItems(lunchMenu, '#dt-lunch');
+loadItems(dinnerMenu, '#dt-dinner');
+loadItems(happyHour, '#m-happyhour');
+loadItems(lunchMenu, '#m-lunch');
+loadItems(dinnerMenu, '#m-dinner');
 
-//window event listener for resizing event
+
+//window event listener for resizing event, will run loadMenuPage function on resize
 var windowSize = window.innerWidth;
 window.addEventListener('resize', function () {
+    hideMenus();
     windowSize = window.innerWidth;
-    loadMenuPage();//when this runs the content inside keeps adding additional event listeners without removing them, so show hide happens on the click event of those buttons, should I just add different menu buttons containers for mobile instead of messing with the click event on the same buttons
-    console.log('window resize and function')
 
 });
-loadMenuPage();
-function loadMenuPage() {
 
-    //hide all containers
-    $('#dt-happyhour').hide();
-    $('#dt-lunch').hide();
-    $('#dt-dinner').hide();
-    $('#m-happyhour').hide();
-    $('#m-lunch').hide();
-    $('#m-dinner').hide();
+//for desktop view add this event listener to buttons
+var menuNav = document.getElementsByClassName('menu-buttons')[0];
+menuNav.addEventListener('click', function (e) {
+    var navSelection = e.target;
+    var menuType = e.target.getAttribute('data-is');
+    var mySiblings = ($(navSelection).parent().siblings());
+    var siblingOne = mySiblings[0].getElementsByTagName('h3')[0].getAttribute('data-is');
+    var siblingTwo = mySiblings[1].getElementsByTagName('h3')[0].getAttribute('data-is');
 
-
-
-    if (windowSize >= desktopView) {
-        loadItems(happyHour, '#dt-happyhour');
-        loadItems(lunchMenu, '#dt-lunch');
-        loadItems(dinnerMenu, '#dt-dinner');
-
-        //fix event listener to scroll to the top of the DIV when it is shown
-
-        var menuNav = document.getElementsByClassName('menu-buttons')[0]
-        //add event click event listener to the parent of the menu types selection
-
-        menuNav.addEventListener('click', function (e) {
-            console.log('added event listner to desktop')
-            //this needs an if else checking window size for what event listener to add
-            var navSelection = e.target;
-            var menuType = e.target.getAttribute('data-is');
-            var mySiblings = ($(navSelection).parent().siblings());
-            var siblingOne = mySiblings[0].getElementsByTagName('h3')[0].getAttribute('data-is');
-            var siblingTwo = mySiblings[1].getElementsByTagName('h3')[0].getAttribute('data-is');
-
-            function hideSiblings() {
-                $('#dt-' + siblingOne).hide();
-                $('#dt-' + siblingTwo).hide();
-            }
-            switch (menuType) {
-                case 'lunch':
-                    $('#dt-' + menuType).show();
-                    hideSiblings();
-                    break;
-                case 'happyhour':
-                    $('#dt-' + menuType).show()
-                    hideSiblings();
-                    break;
-                case 'dinner':
-                    $('#dt-' + menuType).show();
-                    hideSiblings();
-                    break;
-                default: console.log('Menu button failing to show menu items, see menu.js');
-            }
-            if ($('#dt-' + menuType)) {
-                $('html, body').animate({
-
-                    scrollTop: $('#dt-' + menuType).offset().top - 200
-                }, 1000);
-
-            }
-
-
-        });
+    function hideSiblings() {
+        $('#dt-' + siblingOne).hide();
+        $('#dt-' + siblingTwo).hide();
     }
-
-
-    //MEDIA SCREEN CLICK SHOW HIDE LOAD MENU
-
-    if (windowSize <= mobileView) {
-        loadItems(happyHour, '#m-happyhour');
-        loadItems(lunchMenu, '#m-lunch');
-        loadItems(dinnerMenu, '#m-dinner');
-
-        var menuNav = document.getElementsByClassName('menu-buttons')[0]
-        //add event click event listener to the parent of the menu types selection
-
-        menuNav.addEventListener('click', function (e) {
-            console.log('added event listner to mobile')
-            //this needs an if else checking window size for what event listener to add
-            var navSelection = e.target;
-            var menuType = e.target.getAttribute('data-is');
-            var mySiblings = ($(navSelection).parent().siblings());
-            var siblingOne = mySiblings[0].getElementsByTagName('h3')[0].getAttribute('data-is');
-            var siblingTwo = mySiblings[1].getElementsByTagName('h3')[0].getAttribute('data-is');
-
-            // function hideSiblings() {
-            //     $('#m-' + siblingOne).hide();
-            //     $('#m-' + siblingTwo).hide();
-            // }
-            switch (menuType) {
-                case 'lunch':
-                    $('#m-' + menuType).toggle();
-                    //  hideSiblings();
-                    break;
-                case 'happyhour':
-                    $('#m-' + menuType).toggle()
-                    // hideSiblings();
-                    break;
-                case 'dinner':
-                    $('#m-' + menuType).toggle()
-                    // hideSiblings();
-                    break;
-                default: console.log('Menu button failing to show menu items, see menu.js');
-            }
-            // if ($('#m-' + menuType)) {
-            //     $('html, body').animate({
-
-            //         scrollTop: $('#-' + menuType).offset().top - 200
-            //     }, 1000);
-
-            // }
-
-
-        });
+    switch (menuType) {
+        case 'lunch':
+            $('#dt-' + menuType).show();
+            hideSiblings();
+            break;
+        case 'happyhour':
+            $('#dt-' + menuType).show()
+            hideSiblings();
+            break;
+        case 'dinner':
+            $('#dt-' + menuType).show();
+            hideSiblings();
+            break;
+        default: console.log('Menu button failing to show menu items, see menu.js');
     }
+    if ($('#dt-' + menuType)) {
+        $('html, body').animate({
 
-}
+            scrollTop: $('#dt-' + menuType).offset().top - 200
+        }, 1000);
+
+    }
+});
+
+
+var menuNav = document.getElementsByClassName('media-menu-buttons')[0]
+menuNav.addEventListener('click', function (e) {
+    console.log('sanity')
+    var navSelection = e.target;
+    var menuType = e.target.getAttribute('data-is');
+    console.log(menuType)
+    var mySiblings = ($(navSelection).parent().siblings());
+    console.log(mySiblings)
+    switch (menuType) {
+        case 'lunch':
+            $('#m-' + menuType).toggle();
+            console.log('toggled')
+            break;
+        case 'happyhour':
+            $('#m-' + menuType).toggle()
+            break;
+        case 'dinner':
+            $('#m-' + menuType).toggle()
+            break;
+        default: console.log('Menu button failing to show menu items, see menu.js');
+    }
+});
+
+
+
+
 
 
 
